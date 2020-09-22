cd8628fde94390c15654ea7eab746c97b4bb7d00
Lucas Gabriel <lucas.26.04@hotmail.com>
2020-09-22 15:54:55 -0300

    Exercicio 2.4 Unidade operações

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 
-// 2.3 Utilizando o find
-// Crie uma variável que procura por um usuário que trabalhe na empresa Google: undefined
-
+// 2.4 Unindo operações
+// Multiplique a idade de todos usuários por dois e depois realize um filtro nos usuários que possuem no máximo 50 anos:
 
 const usuarios = [
     { nome: 'Diego', idade: 23, empresa: 'Rocketseat' },
     { nome: 'Gabriel', idade: 15, empresa: 'Rocketseat' },
     { nome: 'Lucas', idade: 30, empresa: 'Facebook' },
-
    ];
 
-   const Google = usuarios.find(usuarios => usuarios.empresa === "Google")
-   console.log(Google);
+
+   const  calculo  =  usuarios
+   .map(usuario =>({ ... usuario, idade: usuario.idade * 2}))
+   .filter(usuario  => usuario.idade <= 50 );
+ 
+ console.log(calculo);
