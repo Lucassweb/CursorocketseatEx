b0620b63cffa4c5c94dea448bde1a94883b6cbc6
Lucas Gabriel <lucas.26.04@hotmail.com>
2020-09-22 19:50:18 -0300

    Exercicio 3.1 Arrow Function

--- main.js
+++ main.js
@@ -1,16 +1,8 @@
 
-// 2.4 Unindo operações
-// Multiplique a idade de todos usuários por dois e depois realize um filtro nos usuários que possuem no máximo 50 anos:
+// Converta as funções nos seguintes trechos de código em Arrow Functions:
 
-const usuarios = [
-    { nome: 'Diego', idade: 23, empresa: 'Rocketseat' },
-    { nome: 'Gabriel', idade: 15, empresa: 'Rocketseat' },
-    { nome: 'Lucas', idade: 30, empresa: 'Facebook' },
-   ];
+const arr = [1, 2, 3, 4, 5];
 
+const newArr = arr.map(item => item + 10);
 
-   const  calculo  =  usuarios
-   .map(usuario =>({ ... usuario, idade: usuario.idade * 2}))
-   .filter(usuario  => usuario.idade <= 50 );
- 
- console.log(calculo);
+console.log(newArr);
